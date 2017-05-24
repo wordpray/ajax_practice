8f115045ab9cbe0bf43abc8a4390bf4bfeffa487
yoshi <yoshi@tech-camp-9-021-no-MacBook-Air.local>
2017-05-24 17:12:42 +0900

    APIキーを使って天気予報を取得することができた

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// ここにAPIのキーを入れてね
+const API_KEY = '';
 
+$(function(){
+  $('#weather-form').on('submit',function(e){
+    var cityName = $(this).find('#weather-form-city').prop('value');
+    $.ajax('http://api.openweathermap.org/data/2.5/weather?APPID=' + API_KEY + '&q=' + cityName)
+      .done(function(data) {
+      if (data.cod == 200){
+        $('#form-error').css('display','none');
+        $("#result-city-name").text(data.name);
+        $("#result-temp").text(Math.round(data.main.temp - 273));
+        $("#result-weather").text(data.weather[0].main);
+        $("#result-datetime").text(Date(data.dt));
+        $("#form-result").css('display','block');
+      }else{
+        $("#form-error").css('display','block');
+        $("#form-result").css('display','none');
+      }
+      //成功時の処理
+    })
+    .fail(function(date){
+      //失敗時の処理
+        $('#form-spinner').css('display','none');
+        alert('something wrong occurred.');
+    });
+    e.preventDefault();
+  });
+});
